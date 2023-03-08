4371045e2f4499b689ec1166a6ebefa57117900e
GUIEEN(Seungyong Kwak) <guieenoutis@gmail.com>
2023-03-08 12:06:14 +0900

    zsh:Added python config

---
+++ zsh/config/python.zsh
@@ -0,0 +1,7 @@
+# https://github.com/pyenv/pyenv#set-up-your-shell-environment-for-pyenv
+# Define environment variable PYENV_ROOT to point to the path where Pyenv will store its data. $HOME/.pyenv is the default.
+export PYENV_ROOT="$HOME/.pyenv"
+# to add pyenv to your path and to initialize pyenv/pyenv-virtualenv auto completion. 
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
