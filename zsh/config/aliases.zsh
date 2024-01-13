ddddd1fe73d44bc745d8fd951bbe7012c7031ba9
GUIEEN(Seungyong Kwak) <guieenoutis@gmail.com>
2024-01-14 05:39:49 +0900

    Setup git-delta lazy-git

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -2,6 +2,8 @@
 # - Alias
 ############################################
 alias nv="nvim"
+alias vim='nvim --listen /tmp/nvim-server.pipe'
+
 
 # Prevent accidental rm -rf /*
 #alias rm='rm --preserve-root'
