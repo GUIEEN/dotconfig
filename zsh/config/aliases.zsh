42f834db6315be34a449346dbae57b3f026a8add
GUIEEN(Seungyong Kwak) <guieenoutis@gmail.com>
2024-02-05 10:38:28 +0900

    zsh: updated alias

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -4,6 +4,8 @@
 alias nv="nvim"
 alias vim='nvim --listen /tmp/nvim-server.pipe'
 
+alias pwd="pwd | sed \"s,^$HOME,~,\"" # relative from home by default
+alias pwda="pwd" # absolute
 
 # Prevent accidental rm -rf /*
 #alias rm='rm --preserve-root'
